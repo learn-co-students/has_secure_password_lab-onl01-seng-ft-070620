40b2e2bffa541c53e6558ced210207a9a1285e4b
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-09-26 20:43:34 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/secret' => 'secrets#show'
+  get '/secret' => 'secrets#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
